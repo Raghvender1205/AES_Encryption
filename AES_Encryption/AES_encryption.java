9769143be552495f39aae1ad39ebb619de251e70
Raghvender <53270689+Raghvender1205@users.noreply.github.com>
2020-09-19 14:10:27 +0530

    Add files via upload

---
+++ AES_Encryption/AES_encryption.java
@@ -0,0 +1,62 @@
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.apache.commons.codec.binary.Base64;
+
+/**
+ * AES encryption
+ */
+
+public class AES {
+    private static SecretKeySpec secretKey;
+    private static byte[] key;
+
+    private static String decryptedString;
+    private static String encryptedString;
+
+    private static void setKey(String myKey) {
+        MessageDigest sha = null;
+        try {
+            key = myKey.getBytes("UTF-8");
+            System.out.println(key.length);
+            sha = MessageDigest.getInstance("SHA-1");
+            key = sha.digest(key);
+            key = Arrays.copyOf(key, 16);
+            System.out.println(key.length);
+            System.out.println(new String(key, "UTF-8"));
+            secretKey = new SecretKeySpec(key, "AES");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getDecryptedString() {
+        return decryptedString;
+    }
+
+    public static void setDecryptedString(String decryptedString) {
+        AES.decryptedString = decryptedString;
+    }
+
+    public static String setEncryptedString() {
+        return encryptedString;
+    }
+
+    public static String encrypt(String strToEncrypt) {
+        try {
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+
+            setEncryptedString(Base64.encodeBase64String(cipher.doFinal(strToEncrypt.getBytes("UTF-8"))));
+        } catch (Exception e) {
+
+        }
+    }
+}
